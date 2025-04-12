ad0b9cb5bf2ee9c3e5c07a4308650708efb07fba
Hannes Harnisch <hannes.harnisch@fu-berlin.de>
2025-04-12 05:10:21 +0200

    feat: updated for new chezmoi version and streamlined setup

---
+++ home/.chezmoiscripts/.install-password-manager.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# exit immediately if password-manager-binary is already in $PATH
+
+type op >/dev/null 2>&1 && exit
+echo "Installing Passwordmanager"
+case "$(uname -s)" in
+Darwin)
+    # Installing Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Installing 1password
+    /opt/homebrew/bin/brew install --cask 1password
+    /opt/homebrew/bin/brew install --cask 1password-cli
+    eval $(op signin)
+    echo "Passwordmanager all setup"
+    ;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
